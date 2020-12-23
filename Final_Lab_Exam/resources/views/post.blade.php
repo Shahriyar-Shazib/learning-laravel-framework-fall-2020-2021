ab89e264c552207cbd6ded8a6d4b50e418b472f7
Shahriyar-Shazib <18-36610-1@student.aiub.edu>
2020-12-23 13:58:54 +0600

    last commit

--- Final_Lab_Exam/resources/views/post.blade.php
+++ Final_Lab_Exam/resources/views/post.blade.php
@@ -4,6 +4,25 @@
 	<title>Post</title>
 </head>
 <body>
+	<table>
+	<th>
+		<td>companey</td>
+		<td>title</td>
+		<td>location</td>
+		<td>sal</td>
+		<td>Operation</td>
+	</th>
+		@foreach($i=0;i>count (post);i++)
+		<tr>
+			<td>{{post[$i][CompanyName]}}</td>
+			<td>{{post[$i][Job_Title]}}</td>
+			<td>{{post[$i][location]}}</td>
+			<td>{{post[$i][Salary]}}</td>
+			<td><a href="">edit</a><a href="">Delete</a><a href="">Update</a></td>
+		</tr>
+	</table>
+	
+
 
 </body>
 </html>
